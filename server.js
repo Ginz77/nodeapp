13365f0ef85233158c2bf52070d8cbf96783f115
Ginz77 <ginz1977@gmail.com>
2023-01-16 20:26:04 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    let path = './views/';
+
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    } // Route pages
+
+    res.setHeader('Content-Type', 'text/html');
+    // Set header content type as HTML
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    }); // Read the response path files
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Request successful');
+}); // Listen to check is request is successful
